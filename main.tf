27595c00cc9228d825878c12b8509733cfee69cf
Matt Todd <matt.c.todd@gmail.com>
2020-05-24 05:05:29 -0500

    added api load balancer

--- main.tf
+++ main.tf
@@ -151,3 +151,38 @@ resource "aws_security_group" "ec2-sg" {
 
   tags = { Name = "${var.app_tag}-ec2" }
 }
+
+# Load Balancer
+
+resource "aws_lb" "api-lb" {
+  name = "${var.app_tag}-api-lb"
+  internal = false
+  load_balancer_type = "application"
+
+  subnets         = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+  security_groups = [aws_security_group.lb-sg.id]
+
+  tags = { Name = "${var.app_tag}-lb" }
+}
+
+# Target Group
+
+resource "aws_lb_target_group" "api-tg" {
+  name     = "${var.app_tag}-api-tg"
+  port     = 5000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+}
+
+# Listener
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.api-lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.api-tg.arn
+  }
+}
