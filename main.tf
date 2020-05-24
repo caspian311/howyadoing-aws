5ec9442793b3f45e03ea905832b846bca2f440e2
Matt Todd <matt.c.todd@gmail.com>
2020-05-24 04:43:47 -0500

    added security groups for LB and EC2 instances

--- main.tf
+++ main.tf
@@ -44,17 +44,16 @@ data "aws_availability_zones" "available" {}
 
 # Networking
 
-# NETWORKING #
 resource "aws_vpc" "vpc" {
   cidr_block = var.network_address_space
 
-  tags = merge(local.common_tags, { Name = "${var.environment_tag}-vpc" })
+  tags = { Name = "${var.app_tag}-vpc" }
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 
-  tags = merge(local.common_tags, { Name = "${var.environment_tag}-igw" })
+  tags = { Name = "${var.app_tag}-igw" }
 
 }
 
@@ -64,7 +63,7 @@ resource "aws_subnet" "subnet1" {
   map_public_ip_on_launch = "true"
   availability_zone       = data.aws_availability_zones.available.names[0]
 
-  tags = merge(local.common_tags, { Name = "${var.environment_tag}-subnet1" })
+  tags = { Name = "${var.app_tag}-subnet1" }
 
 }
 
@@ -74,7 +73,7 @@ resource "aws_subnet" "subnet2" {
   map_public_ip_on_launch = "true"
   availability_zone       = data.aws_availability_zones.available.names[1]
 
-  tags = merge(local.common_tags, { Name = "${var.environment_tag}-subnet2" })
+  tags = { Name = "${var.app_tag}-subnet2" }
 
 }
 
@@ -87,7 +86,7 @@ resource "aws_route_table" "rtb" {
     gateway_id = aws_internet_gateway.igw.id
   }
 
-  tags = merge(local.common_tags, { Name = "${var.environment_tag}-rtb" })
+  tags = { Name = "${var.app_tag}-rtb" }
 
 }
 
@@ -99,4 +98,56 @@ resource "aws_route_table_association" "rta-subnet1" {
 resource "aws_route_table_association" "rta-subnet2" {
   subnet_id      = aws_subnet.subnet2.id
   route_table_id = aws_route_table.rtb.id
+}
+
+# Security Groups
+
+resource "aws_security_group" "lb-sg" {
+  name   = "${var.app_tag}_lb_sg"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "${var.app_tag}-lb" }
+}
+
+resource "aws_security_group" "ec2-sg" {
+  name   = "${var.app_tag}_ec2_sg"
+  vpc_id = aws_vpc.vpc.id
+
+  # maybe restrict to network space and create a jump box later
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.network_address_space]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "${var.app_tag}-ec2" }
 }
